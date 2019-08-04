9e40acbf5bb2351e0ed8e36dbb41990aaae15c00
Nick Carroll <ncarroll@nd.edu>
2019-08-03 21:22:04 -0500

    Added some basics to gamestate class

---
+++ GMTK2019/Assets/GameState.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState : MonoBehaviour
+{
+    //ArrayList<ArrayList<int>> boardState;
+    public int[][] boardState = new int[9];
+
+
+
+    ArrayList 
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i =0; i < 9; i++)
+        {
+            for (int j = 0; i < 9; i++)
+            {
+                 // create a tile located at I,J
+
+            }
+        }
+    }
+
+    enum phase { TITLE, DESIGN, BATTLE}
+    private int currentPhase = (int) phase.TITLE;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (currentPhase == (int)phase.TITLE)
+        {
+
+        }
+        else if (currentPhase == (int)phase.DESIGN)
+        {
+
+        }
+        else if (currentPhase == (int)phase.BATTLE)
+        {
+
+        }
+
+        /// show a title screen
+        ///
+
+        /// design a ship
+        ///
+
+        /// first create a board
+        ///
+
+        /// design phase
+        ///
+        //if 
+
+
+    }
+}
